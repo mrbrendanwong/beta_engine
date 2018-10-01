37d18ccba8ee3a05854fe2fed1cf4551f6242731
mrbrendanwong <brendonydwong@gmail.com>
2018-09-30 18:27:25 -0700

    Add scene globals

--- src/components/Scene.java
+++ src/components/Scene.java
@@ -2,6 +2,27 @@ package components;
 
 import lib.Node;
 
-public abstract class Scene extends Node {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Scene extends Node {
+    public String sceneName;
+    public List<String> texts = new ArrayList<>();
+    public List<Choice> choices = new ArrayList<>();
+    public int timer;
+
+    public String bgmFile;
+    public String soundFile;
+    public String pictureFile;
+    public String picturePosition;
+
+    @Override
+    public void parse() {
+
+    }
+
+    @Override
+    public void evaluate() {
+
+    }
 }
