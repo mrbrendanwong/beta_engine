0e449c69ea30fcf26501cad1411c69273679a473
Thomas Zhou <thethomaszhou@gmail.com>
2018-10-10 01:00:31 -0700

    WIP some disgusting game frame update implementation on button click - Missing death and end scenes

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,22 +2,36 @@ package client;
 
 import components.Game;
 import components.Scene;
+import lib.Node;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class Client {
+public class Client implements ActionListener {
+    private int NUM_BUTTONS = 4;
+
     private Game game;
     private JFrame frame;
-    public Scene currScene;
+    private JPanel statusPanel;
+    private JPanel mainPanel;
+    private JPanel interactionPanel;
+    private Scene currScene;
+    private List<JButton> buttons = new ArrayList<>();
 
     public Client(Game gameObj) {
         game = gameObj;
 
         // Create the JFrame
         frame = new JFrame(game.title);
+        frame.pack();
         frame.setSize(1280, 720);
         frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // Create and set a layout with 3 rows
@@ -26,32 +40,32 @@ public class Client {
         // 3rd is the button and text panel
         GridLayout layout = new GridLayout(3, 1);
         frame.setLayout(layout);
-        JPanel statusPanel = new JPanel();
-        frame.add(statusPanel);
-        JPanel mainPanel = new JPanel();
-        frame.add(mainPanel);
-        JPanel interactionPanel = new JPanel();
+
+        statusPanel = new JPanel();
+        mainPanel = new JPanel();
+        interactionPanel = new JPanel();
         GridLayout buttonLayout = new GridLayout(2, 2);
         interactionPanel.setLayout(buttonLayout);
+
+        frame.add(statusPanel);
+        frame.add(mainPanel);
         frame.add(interactionPanel);
 
         // Create buttons
-        JButton choice1 = new JButton("Choice 1");
-        JButton choice2 = new JButton("Choice 2");
-        JButton choice3 = new JButton("Choice 3");
-        JButton choice4 = new JButton("Choice 4");
+        for (int i = 0; i < NUM_BUTTONS; i++) {
+            buttons.add(new JButton());
+            buttons.get(i).setActionCommand(String.format("%d", i));
+            buttons.get(i).addActionListener(this);
+        }
 
-        // Read Start scene
-
-        // Set curr scene
+        // Set start scene to the current scene
+        currScene = game.startScene;
 
         // Add buttons and stuff
-        interactionPanel.add(choice1);
-        interactionPanel.add(choice2);
-        interactionPanel.add(choice3);
-        interactionPanel.add(choice4);
-
-        // On click actions, update references
+        for (int i = 0; i < currScene.choices.size(); i++) {
+            buttons.get(i).setText(currScene.choices.get(i).text);
+            interactionPanel.add(buttons.get(i));
+        }
     }
 
     // Temp
@@ -60,8 +74,29 @@ public class Client {
     }
 
     // Updates current elements of game on the frame
-    private void updateFrame() {
-
+    private void updateFrame(int buttonNum) {
+        String nextScene = currScene.choices.get(buttonNum).next;
+        for (Node node : game.storyScenes) {
+            Scene storyScene = (Scene) node;
+            if (Objects.equals(storyScene.name, nextScene)) {
+                currScene = storyScene;
+            }
+        }
+        updateButtons();
     }
 
+    private void updateButtons() {
+        interactionPanel.removeAll();
+        for (int i = 0; i < currScene.choices.size(); i++) {
+            buttons.get(i).setText(currScene.choices.get(i).text);
+            interactionPanel.add(buttons.get(i));
+        }
+        interactionPanel.revalidate();
+        interactionPanel.repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        updateFrame(Integer.parseInt(e.getActionCommand()));
+    }
 }
